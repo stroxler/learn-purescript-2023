ec41821d1f032caf787c5f88be3966b8055f3a7d
Steven Troxler <steven.troxler@gmail.com>
2023-06-18 08:40:08 -0700

    Struggling with mutually recursive parsers
    Mutually recursive functions aren't a problem in purescript, but
    mutually recursive parsers are. The data definitions reference one
    another, which isn't allowed in the javascript runtime. I tried
    wrapping the recursive parsers in delayed evaluation but that doesn't
    actually work... it delays evaluation, but only until we try to
    instantiate the parses, and then it tries to create them, producing
    the same cycle that the compiler forbids without the delayed
    evaluation and thereby blowing the stack.
    
    I'm still trying to understand how to use the suggestions in
    https://github.com/purescript/documentation/blob/master/errors/CycleInDeclaration.md

--- parser-for-json/src/Main.purs
+++ parser-for-json/src/Main.purs
@@ -1,4 +1,16 @@
 module Main
+  ( Json(..)
+  , main
+  , parseCharacter
+  , parseJBoolean
+  , parseJNull
+  , parseJNumber
+  , parseJString
+  , parseJson
+  , parseNatural
+  , parseRawNull
+  , parseSymbol
+  )
   where
 
 import Prelude
@@ -7,14 +19,14 @@ import Control.Alt ((<|>))
 import Control.Monad.Except (throwError)
 import Data.Foldable (fold)
 import Data.Generic.Rep (class Generic)
-import Data.List (List)
+import Data.List (List(..))
 import Data.Maybe (Maybe(..))
 import Data.Number as Number
 import Data.Show.Generic (genericShow)
 import Data.Tuple (Tuple)
 import Effect (Effect)
 import Effect.Class.Console (logShow)
-import Parser (Parser, consume1, createInitialState, many1, many, matches, runParser)
+import Parser (Parser, consume1, createInitialState, many, many1, matches, runParser)
 
 
 data Json
@@ -22,8 +34,8 @@ data Json
   | JNumber Number
   | JBoolean Boolean
   | JString String
-  | JArray (Array Json)
-  | JAssoc (Array (Tuple String Json))
+  | JArray (List Json)
+  | JAssoc (List (Tuple String Json))
 
 
 derive instance genericJNode ∷ Generic Json _
@@ -54,6 +66,14 @@ parseCharacter = do
         "n" -> pure "\n"
         _ -> pure nextChar
     _ -> pure char
+
+
+-- Note the delayed evaluation here: mutually recursive functions are allowed,
+-- but mutually recursive *data* definitions are not because the javascript runtime
+-- cannot evaluate a forward reference at the top-level (this is unlike in Haskell,
+-- where laziness + compiled code makes this possible).
+parseJson :: Unit -> Parser Json
+parseJson _ = parseJNull <|> parseJBoolean <|> parseJNumber <|> parseJString <|> (parseJArray unit)
 
 
 parseJString :: Parser Json
@@ -99,11 +119,33 @@ parseJBoolean :: Parser Json
 parseJBoolean = (\raw -> JBoolean $ raw == "true") <$> (matches "true" <|> matches "false")
 
 
+parseJArray :: Unit -> Parser Json
+parseJArray _ = JArray <$> (matches "[" *> parseList <* matches "]")
+  where
+  parseList :: Parser (List Json)
+  parseList = parseListAtLeast1 <|> pure Nil
+  parseListAtLeast1 :: Parser (List Json)
+  parseListAtLeast1 = do
+    first <- parseJson unit
+    rest <- parseRestOfList <|> pure Nil
+    pure $ Cons first rest
+  parseRestOfList :: Parser (List Json)
+  parseRestOfList = do
+    _ <- matches ","
+    _ <- matches " " <|> pure ""
+    parseListAtLeast1
+
+
+
+
 main :: Effect Unit
 main = do
-    logShow $ runParser (createInitialState "\"this is a\\ string!\\\"\\\\\\n\" and this is the rest") parseJString
-    logShow $ runParser (createInitialState "-178 and some more") parseJNumber
-    logShow $ runParser (createInitialState "087 and some more") parseJNumber
-    logShow $ runParser (createInitialState "null and some more") parseJNull
-    logShow $ runParser (createInitialState "true and some more") parseJBoolean
-    logShow $ runParser (createInitialState "false and some more") parseJBoolean
+    let _ = parseJson unit
+    logShow "hello"
+    --logShow $ runParser (createInitialState "\"this is a\\ string!\\\"\\\\\\n\" and this is the rest") (parseJson unit)
+    --logShow $ runParser (createInitialState "-178 and some more") (parseJson unit)
+    --logShow $ runParser (createInitialState "087 and some more") (parseJson unit)
+    --logShow $ runParser (createInitialState "null and some more") (parseJson unit)
+    --logShow $ runParser (createInitialState "true and some more") (parseJson unit)
+    --logShow $ runParser (createInitialState "false and some more") (parseJson unit)
+    --logShow $ runParser (createInitialState "[false, null, \"hi\", -86] and some more") (parseJson unit)
